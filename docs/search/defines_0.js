55cc6a6fd607c7d15ace553561e454c6d47eefe3
DiarrheaMcgee <DiarrheaMcgee@users.noreply.github.com>
2025-03-27 03:39:51 +0000

    deploy: c24764c79ba0c60e1ee42c93fd3f81a39220c862

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_5f_5fuse_5fposix199309_0',['__USE_POSIX199309',['../RGFW_8h.html#a4ad9e77eef1d695b1a5cca6a76116bcf',1,'RGFW.h']]]
+  ['_5f_5fuse_5fposix199309_0',['__USE_POSIX199309',['../RGFW_8h.html#a4ad9e77eef1d695b1a5cca6a76116bcf',1,'RGFW.h']]],
+  ['_5frgfw_5finc_5fstring_1',['_RGFW_INC_STRING',['../RGFW_8h.html#acc15693b89ef93d9ce3e0b63fe924a01',1,'RGFW.h']]]
 ];
